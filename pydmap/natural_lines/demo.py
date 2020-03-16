52fb5a8ac18234c53c1dd46c71994e004404ba6d
Dave Beck <beck.dac@live.com>
2020-03-16 12:51:57 -0700

    removed old ;

--- pydmap/natural_lines/demo.py
+++ pydmap/natural_lines/demo.py
@@ -6,13 +6,13 @@ def demo(ctx):
     a = 10
     w = 100
     ws = w * .9
-    ctx.translate(520, 0);
+    ctx.translate(520, 0)
     ctx.scale(-1, 1)
     print_points = False
     random.seed()
     ctx.set_source_rgba(0, 0, 0, 1)
-    rect(ctx, a, a, ws, ws, squiggle_strength=10, print_points=print_points);
-    rect(ctx, a + w, a, ws, ws, squiggle_strength=7, print_points=print_points);
-    rect(ctx, a + 2*w, a, ws, ws, squiggle_strength=5, print_points=print_points);
-    rect(ctx, a + 3*w, a, ws, ws, squiggle_strength=2, print_points=print_points);
-    rect(ctx, a + 4*w, a, ws, ws, squiggle_strength=1, print_points=print_points);
+    rect(ctx, a, a, ws, ws, squiggle_strength=10, print_points=print_points)
+    rect(ctx, a + w, a, ws, ws, squiggle_strength=7, print_points=print_points)
+    rect(ctx, a + 2*w, a, ws, ws, squiggle_strength=5, print_points=print_points)
+    rect(ctx, a + 3*w, a, ws, ws, squiggle_strength=2, print_points=print_points)
+    rect(ctx, a + 4*w, a, ws, ws, squiggle_strength=1, print_points=print_points)
