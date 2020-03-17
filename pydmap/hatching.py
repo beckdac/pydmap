3af464b2956180e6247f43601e1080f3c5975e01
Dave Beck <beck.dac@live.com>
2020-03-16 18:17:27 -0700

    small progress to refining hatching function

--- pydmap/hatching.py
+++ pydmap/hatching.py
@@ -6,26 +6,31 @@ import numpy as np
 from .natural_lines import line
 
 
+CELL_SIZE = 100
+
 def hatch_cell(ctx, xcell, ycell, lines_per_cell=5, squiggle_strength=5, seed=None, print_points=False):
     ctx.save()
 
     hatch_dir_x = xcell % 2
     hatch_dir_y = ycell % 2
-    mid_x = xcell * 100 + 50
-    mid_y = ycell * 100 + 50
+    mid_x = xcell * CELL_SIZE + CELL_SIZE / 2
+    mid_y = ycell * CELL_SIZE + CELL_SIZE / 2
 
-    for x in np.linspace(0., 100., lines_per_cell + 2)[1:-1]:
-        xi = xcell * 100. + x
-        yi = ycell * 100.
-        xi1 = xi + random.random() * 10. + 5.
-        yi1 = yi + random.random() * 10. + 10.
-        xi2 = xi + random.random() * 10. - 5.
-        yi2 = yi + 100 - random.random() * 10. - 10.
+    #ctx.translate(-mid_x, -mid_y)
+    #ctx.rotate(45. * math.pi/180.)
+
+    #line(ctx, mid_x, mid_y, 100, 100)
+
+    for x in np.linspace(0., CELL_SIZE, lines_per_cell + 2)[1:-1]:
+        xi = xcell * CELL_SIZE + x
+        yi = ycell * CELL_SIZE
+        xi1 = xi + random.random() * 10. + CELL_SIZE / 2 / 10.
+        yi1 = yi + random.random() * 10. + CELL_SIZE / 10.
+        xi2 = xi + random.random() * 10. - CELL_SIZE / 2/ 10.
+        yi2 = yi + CELL_SIZE - random.random() * 10. - CELL_SIZE / 10.
         line(ctx, xi1, yi1, xi2, yi2, squiggle_strength=squiggle_strength, print_points=print_points, seed=seed)
 
-    ctx.translate(-mid_x, -mid_y)
-    ctx.rotate(-math.pi/4)
-    ctx.translate(mid_x, mid_y)
+    #ctx.translate(mid_x, mid_y)
 
     ctx.stroke()
     ctx.restore()
@@ -34,10 +39,10 @@ def hatch_cell(ctx, xcell, ycell, lines_per_cell=5, squiggle_strength=5, seed=No
 def demo(ctx, height, width):
     print_points = False
     ctx.set_source_rgba(0, 0, 0, 1)
-    ctx.move_to(0, 100)
-    ctx.line_to(200, 100)
-    ctx.move_to(100, 0)
-    ctx.line_to(100, 200)
+    ctx.move_to(0, CELL_SIZE)
+    ctx.line_to(200, CELL_SIZE)
+    ctx.move_to(CELL_SIZE, 0)
+    ctx.line_to(CELL_SIZE, 200)
     ctx.stroke()
     hatch_cell(ctx, 0, 0)
     #hatch_cell(ctx, 1, 1)
