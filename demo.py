35e66c60b2fb8c02eed6110e1839a985c1bf2b2e
Dave Beck <beck.dac@live.com>
2020-03-17 00:05:32 -0700

    added grid demo

--- demo.py
+++ demo.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import pydmap
-from pydmap import hatching, natural_lines
+from pydmap import hatching, natural_lines, grid
 
 
 def main():
     width = 600
-    height = 200
+    height = 400
     pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=hatching.demo)
     pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=natural_lines.demo)
+    pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=grid.demo)
 
 if __name__== "__main__":
   main()
