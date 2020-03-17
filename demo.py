e8f255bab6cc63bece46c221a3a5e16d80354db7
Dave Beck <beck.dac@live.com>
2020-03-16 22:02:52 -0700

    added call to natural lines demo, note it overwrites

--- demo.py
+++ demo.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import pydmap
-from pydmap import hatching
+from pydmap import hatching, natural_lines
+
 
 def main():
-    width = 400
+    width = 600
     height = 200
     pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=hatching.demo)
+    pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=natural_lines.demo)
 
 if __name__== "__main__":
   main()
