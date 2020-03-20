1b896095a4d36cc1f3f0398a4b04bc8c1016605a
Dave Beck <beck.dac@live.com>
2020-03-20 14:35:41 -0700

    simplified environment and panelled demo

--- demo.py
+++ demo.py
@@ -7,9 +7,9 @@ from pydmap import hatching, natural_lines, grid
 def main():
     width = 600
     height = 400
-    pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=hatching.demo)
-    pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=natural_lines.demo)
-    pydmap.render_png(filename='demo.png', width=width, height=height, draw_func=grid.demo)
+    pydmap.render_png(filename='demo1.png', width=width, height=height, draw_func=hatching.demo)
+    pydmap.render_png(filename='demo2.png', width=width, height=height, draw_func=natural_lines.demo)
+    pydmap.render_png(filename='demo3.png', width=width, height=height, draw_func=grid.demo)
 
 if __name__== "__main__":
   main()
