490544231758b9119d7af54f4baabd3e5da3c31b
Dave Beck <beck.dac@live.com>
2020-03-22 09:35:16 -0700

    pydmap/constants.py

--- pydmap/render.py
+++ pydmap/render.py
@@ -1,24 +1,16 @@
 import cairo
 
-def render_png(filename='pydmap.png', width=512, height=512, pixel_coordinates=True, draw_func=None):
+def render_png(filename='pydmap.png', width=512, height=512, draw_func=None, bg_rgba=(1, 1, 1, 1)):
     surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
     
     context = cairo.Context(surface)
     
-    if pixel_coordinates:
-        pass
-    else:
-        context.scale(width, height)
-    
-    # background is transparent black
-    context.set_source_rgba(0., 0., 0., 0.)
-    # background is white
-    context.set_source_rgba(1., 1., 1., 1.)
+    # paint background
+    context.set_source_rgba(*bg_rgba)
     context.rectangle(0, 0, width, height)
     context.fill()
-    context.stroke()
 
     if draw_func is not None:
         draw_func(context, width, height)
-    
+   
     surface.write_to_png(filename)
